df3f18d2d2a0398b71873394cc569e7720cc8720
bugkaue <alexkauezinho@gmail.com>
2024-08-20 17:18:47 -0300

    feat: add all class

---
+++ ProjetoBase/src/pck/Model/Pessoa.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pck.Model;
+
+public class Pessoa {
+    private String nome;
+    private char sexo;
+    
+    public Pessoa(String nome, char sexo) {
+        this.nome = nome;
+        this.sexo = sexo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public char getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }
+
+    @Override
+    public String toString() {
+        return "Nome: " + nome + ", Sexo: " + sexo;
+    }
+}
